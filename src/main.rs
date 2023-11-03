9c5cff8168ed2c821d25dfac38a5435406002311
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-11-04 00:35:14 +0100

    Introduce `Writing` opaque type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod widget;
+pub mod writing;
 
 use crate::widget::fade;
+use crate::writing::Writing;
 
 use iced::event::{self, Event};
 use iced::font::{self, Font};
@@ -11,10 +13,7 @@ use iced::{executor, Length};
 use iced::{Application, Command, Element, Settings, Subscription, Theme};
 
 use std::env;
-use std::io;
-use std::path::{Path, PathBuf};
-use std::sync::Arc;
-use thiserror::Error;
+use std::path::PathBuf;
 
 fn main() -> iced::Result {
     let Some(filepath) = env::args().skip(1).next() else {
@@ -38,14 +37,8 @@ fn main() -> iced::Result {
 
 enum Kanso {
     Loading,
-    Editing {
-        filepath: PathBuf,
-        content: String,
-        is_dirty: bool,
-    },
-    Errored {
-        error: Error,
-    },
+    Editing { writing: Writing },
+    Errored { error: writing::Error },
 }
 
 struct Flags {
@@ -54,11 +47,11 @@ struct Flags {
 
 #[derive(Debug, Clone)]
 enum Message {
-    Loaded(Result<(PathBuf, Arc<String>), Error>),
+    Loaded(Result<Writing, writing::Error>),
     Write(char),
     Amend,
-    Save(usize),
-    Saved(Result<(), Error>),
+    Save(writing::Version),
+    Saved(Result<(), writing::Error>),
 }
 
 impl Application for Kanso {
@@ -70,7 +63,7 @@ impl Application for Kanso {
     fn new(flags: Self::Flags) -> (Self, Command<Self::Message>) {
         (
             Kanso::Loading,
-            Command::perform(load(flags.filepath), Message::Loaded),
+            Command::perform(Writing::load(flags.filepath), Message::Loaded),
         )
     }
 
@@ -80,12 +73,8 @@ impl Application for Kanso {
 
     fn update(&mut self, message: Message) -> Command<Self::Message> {
         match message {
-            Message::Loaded(Ok((filepath, content))) => {
-                *self = Self::Editing {
-                    filepath,
-                    content: (*content).clone(),
-                    is_dirty: false,
-                };
+            Message::Loaded(Ok(writing)) => {
+                *self = Self::Editing { writing };
 
                 Command::none()
             }
@@ -95,15 +84,11 @@ impl Application for Kanso {
                 Command::none()
             }
             Message::Write(character) => {
-                if let Self::Editing {
-                    content, is_dirty, ..
-                } = self
-                {
-                    content.push(character);
-                    *is_dirty = true;
+                if let Self::Editing { writing } = self {
+                    writing.write(character);
 
                     Command::perform(wait_a_bit(), {
-                        let version = content.len();
+                        let version = writing.version();
                         move |_| Message::Save(version)
                     })
                 } else {
@@ -111,15 +96,11 @@ impl Application for Kanso {
                 }
             }
             Message::Amend => {
-                if let Self::Editing {
-                    content, is_dirty, ..
-                } = self
-                {
-                    content.pop();
-                    *is_dirty = true;
+                if let Self::Editing { writing } = self {
+                    writing.amend();
 
                     Command::perform(wait_a_bit(), {
-                        let version = content.len();
+                        let version = writing.version();
                         move |_| Message::Save(version)
                     })
                 } else {
@@ -127,22 +108,12 @@ impl Application for Kanso {
                 }
             }
             Message::Save(version) => match self {
-                Self::Editing {
-                    filepath,
-                    content,
-                    is_dirty,
-                } if *is_dirty && content.len() == version => {
-                    Command::perform(save(filepath.clone(), content.clone()), Message::Saved)
+                Self::Editing { writing } if writing.is_dirty() && writing.version() == version => {
+                    Command::perform(writing.save(), Message::Saved)
                 }
                 _ => Command::none(),
             },
-            Message::Saved(Ok(())) => {
-                if let Self::Editing { is_dirty, .. } = self {
-                    *is_dirty = false;
-                }
-
-                Command::none()
-            }
+            Message::Saved(Ok(())) => Command::none(),
             Message::Saved(Err(error)) => {
                 *self = Self::Errored { error };
 
@@ -177,17 +148,13 @@ impl Application for Kanso {
     fn view(&self) -> Element<'_, Message> {
         match self {
             Self::Loading => centered("Loading..."),
-            Self::Editing {
-                filepath,
-                content,
-                is_dirty,
-            } => {
+            Self::Editing { writing } => {
                 let writer = fade(
                     container(
-                        text(format!(
-                            "{}_",
+                        text(format!("{}_", {
+                            let content = writing.content();
                             &content[content.len().saturating_sub(1_000)..]
-                        ))
+                        }))
                         .font(Font {
                             family: font::Family::Serif,
                             ..Font::DEFAULT
@@ -200,8 +167,8 @@ impl Application for Kanso {
 
                 let status_bar = row![text(format!(
                     "{}{}",
-                    filepath.as_path().to_str().unwrap_or(""),
-                    if *is_dirty { "*" } else { "" }
+                    writing.filepath().to_str().unwrap_or(""),
+                    if writing.is_dirty() { "*" } else { "" }
                 ))]
                 .padding(20);
 
@@ -228,38 +195,6 @@ fn centered<'a>(content: impl Into<Element<'a, Message>>) -> Element<'a, Message
         .into()
 }
 
-async fn load(filepath: impl AsRef<Path>) -> Result<(PathBuf, Arc<String>), Error> {
-    let path = filepath.as_ref().to_path_buf();
-
-    let exists = tokio::fs::try_exists(filepath).await?;
-
-    let content = if exists {
-        tokio::fs::read_to_string(&path).await?
-    } else {
-        String::new()
-    };
-
-    Ok((path, Arc::new(content)))
-}
-
-async fn save(filepath: impl AsRef<Path>, content: String) -> Result<(), Error> {
-    tokio::fs::write(filepath, content).await?;
-
-    Ok(())
-}
-
 async fn wait_a_bit() {
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-}
-
-#[derive(Debug, Clone, Error)]
-enum Error {
-    #[error("IO operation failed: {0}")]
-    IOFailed(io::ErrorKind),
-}
-
-impl From<io::Error> for Error {
-    fn from(error: io::Error) -> Self {
-        Self::IOFailed(error.kind())
-    }
 }
---
+++ src/writing.rs
@@ -0,0 +1,87 @@
+use std::future::Future;
+use std::io;
+use std::path::{Path, PathBuf};
+
+use thiserror::Error;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Writing {
+    filepath: PathBuf,
+    content: String,
+    version: Version,
+    last_save: Version,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Version(u64);
+
+impl Writing {
+    pub async fn load(filepath: impl AsRef<Path>) -> Result<Self, Error> {
+        let path = filepath.as_ref().to_path_buf();
+        let exists = tokio::fs::try_exists(filepath).await?;
+
+        let content = if exists {
+            tokio::fs::read_to_string(&path).await?
+        } else {
+            String::new()
+        };
+
+        Ok(Self {
+            filepath: path,
+            content,
+            version: Version(0),
+            last_save: Version(0),
+        })
+    }
+
+    pub fn save(&mut self) -> impl Future<Output = Result<(), Error>> {
+        self.last_save = self.version;
+
+        let filepath = self.filepath.clone();
+        let content = self.content.clone();
+
+        async move {
+            tokio::fs::write(filepath, content).await?;
+
+            Ok(())
+        }
+    }
+
+    pub fn filepath(&self) -> &Path {
+        &self.filepath
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+
+    pub fn version(&self) -> Version {
+        self.version
+    }
+
+    pub fn is_dirty(&self) -> bool {
+        self.version != self.last_save
+    }
+
+    pub fn write(&mut self, character: char) {
+        self.content.push(character);
+        self.version = Version(self.version.0 + 1);
+    }
+
+    pub fn amend(&mut self) {
+        let _ = self.content.pop();
+        self.version = Version(self.version.0 + 1);
+    }
+}
+
+#[derive(Debug, Clone, Error)]
+pub enum Error {
+    #[error("IO operation failed: {0}")]
+    IOFailed(io::ErrorKind),
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Self::IOFailed(error.kind())
+    }
+}
