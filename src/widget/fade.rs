194d9c7c0211ce7de7aeb4d2b55930e7a4f7732e
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-11-04 01:15:07 +0100

    Display fading gradient on top of content in `fade` widget

--- src/widget/fade.rs
+++ src/widget/fade.rs
@@ -2,8 +2,9 @@ use iced::advanced;
 use iced::advanced::layout::{self, Layout};
 use iced::advanced::renderer;
 use iced::advanced::widget::{self, Widget};
+use iced::gradient::{self, Gradient};
 use iced::mouse;
-use iced::{Element, Length, Rectangle, Size, Vector};
+use iced::{Background, Color, Degrees, Element, Length, Rectangle, Size, Vector};
 
 pub fn fade<'a, Message>(content: impl Into<Element<'a, Message>>) -> Element<'a, Message>
 where
@@ -81,15 +82,37 @@ where
         cursor: mouse::Cursor,
         viewport: &Rectangle,
     ) {
+        let bounds = layout.bounds();
+        let content_layout = layout.children().next().unwrap();
+        let content_bounds = content_layout.bounds();
+
         self.content.as_widget().draw(
             tree,
             renderer,
             theme,
             style,
-            layout.children().next().unwrap(),
+            content_layout,
             cursor,
             viewport,
         );
+
+        if content_bounds.height > 2.0 * bounds.height / 3.0 {
+            renderer.with_layer(bounds, |renderer| {
+                renderer.fill_quad(
+                    renderer::Quad {
+                        bounds,
+                        border_radius: 0.0.into(),
+                        border_width: 0.0,
+                        border_color: Color::TRANSPARENT,
+                    },
+                    Background::Gradient(Gradient::Linear(
+                        gradient::Linear::new(Degrees(180.0))
+                            .add_stop(0.0, Color::BLACK)
+                            .add_stop(0.7, Color::TRANSPARENT),
+                    )),
+                );
+            });
+        }
     }
 }
 
